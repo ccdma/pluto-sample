ef106629148f6e8997c055c50d463b52f1c40f60
face0u0 <me@face0u0.net>
2021-10-28 11:10:59 +0900

    push

--- ica_ccd.py
+++ ica_ccd.py
@@ -17,45 +17,52 @@ rate = 96000
 upsample_ratio = 150
 targetrate = rate*upsample_ratio
 
+TIME = 15.0
+
 if __name__ == "__main__":
     sdr0 = DEVICES.find("1044734c9605000d15003300deb64fb9ce").create_pluto()
-    sdr1 = DEVICES.find("1044734c96050013f7ff27004a464f13a0").create_pluto()
+    # sdr1 = DEVICES.find("1044734c96050013f7ff27004a464f13a0").create_pluto()
     
-    sdrs = [sdr0, sdr1]
-    for sdr in sdrs:
+    r_sdrs = []
+    t_sdrs = [sdr0]
+    
+    for sdr in r_sdrs+t_sdrs:
         sdr.tx_lo = 920*MHz
         sdr.rx_lo = 920*MHz
         # sdr.tx_hardwaregain_chan0 = 0
         sdr.tx_rf_bandwidth = 100*KHz
         sdr.sample_rate = int(targetrate)
         sdr.rx_rf_bandwidth = 100*KHz
-    
-    r_sdrs = [sdr0]
-    t_sdrs = []
 
     SERIES = len(t_sdrs)
 
-    # S = np.array([seed.chebyt_series(i, 0.1+i/10, 1024*1000) for i in range(SERIES)])
-    fs = int(sdr.sample_rate)
-    N = 1024
-    fc = int(3000000 / (fs / N)) * (fs / N)
-    ts = 1 / float(fs)
-    t = np.arange(0, N * ts, ts)
-    i = np.cos(2 * np.pi * t * fc) * 2 ** 14
-    q = np.sin(2 * np.pi * t * fc) * 2 ** 14
-    S = np.array([i + 1j*q for _ in range(SERIES)])
+    S = np.array([seed.chebyt_series(i, 0.1+i/10, 1024*1000) for i in range(SERIES)])
+    # fs = int(sdr.sample_rate)
+    # N = 1024
+    # fc = int(3000000 / (fs / N)) * (fs / N)
+    # ts = 1 / float(fs)
+    # t = np.arange(0, N * ts, ts)
+    # i = np.cos(2 * np.pi * t * fc) * 2 ** 14
+    # q = np.sin(2 * np.pi * t * fc) * 2 ** 14
+    # S = np.array([i + 1j*q for _ in range(SERIES)])
+
+    S_MOD = []
+    for s in S:
+        resampled = commpy.utilities.upsample(s, upsample_ratio)
+        S_MOD.append(s)
+    S = np.array(S_MOD)
 
     rx_bufs = [[] for _ in r_sdrs]
 
     def send(sdr, s):
         start = time.time()
-        while (time.time() - start) < 2.0:
+        while (time.time() - start) < TIME:
             for idx in range(0, len(s), 1024):
                 sdr.tx(s[idx:idx+1023]*2**14)
 
     def read(sdr, rx_buf):
         start = time.time()
-        while (time.time() - start) < 2.0:
+        while (time.time() - start) < TIME:
             rx_buf.extend(sdr.rx())
 
     threads = []
@@ -71,6 +78,8 @@ if __name__ == "__main__":
     for thread in threads:
         thread.join()
 
+    
+
     # plt.figure()
     # x = np.array(rx_bufs[0][1024*50:1024*101])
     # f, Pxx_den = sig.periodogram(x, fs)
@@ -83,18 +92,18 @@ if __name__ == "__main__":
     # plt.pause(0.05)
     # time.sleep(0.1)
 
-    plt.figure()
-    yf = fft.fft(rx_bufs[0][1024*50:1024*101])
-    yf = fft.fftshift(yf)
-    N = int(len(yf))
-    xf = np.linspace(-targetrate/2.0, targetrate/2.0, N)
-    plt.semilogy(xf, np.abs(yf[:N]), '-b')
+    # plt.figure()
+    # yf = fft.fft(rx_bufs[0][1024*50:1024*101])
+    # yf = fft.fftshift(yf)
+    # N = int(len(yf))
+    # xf = np.linspace(-targetrate/2.0, targetrate/2.0, N)
+    # plt.semilogy(xf, np.abs(yf[:N]), '-b')
 
     # plt.figure()
     # b = np.array(rx_bufs[0][1024*100:1024*101])
     # plt.plot(b.real, b.imag, lw=1)
     # plt.scatter(b.real, b.imag, s=10)
-    plt.show()
+    # plt.show()
 
     # with open('chebyt.csv', 'w') as f:
     #     writer = csv.writer(f)
