1f1791ec0914d77780c98616743f4421d1f943d9
face0u0 <me@face0u0.net>
2021-12-05 16:57:38 +0900

    pusH

--- icaaa.py
+++ icaaa.py
@@ -13,15 +13,15 @@ DEVICES = adiutil.DeviceList()
 
 # np.array()で信号を生成すること！（途中の演算はnp.arrayをを想定している）
 if __name__ == "__main__":
-    SAMPLINGS = 1024*2**4
+    SAMPLINGS = 1024
     # samples = weyl_samples(np.sqrt(0.2), np.sqrt(0.3), SAMPLINGS)
     # samples = np.array([np.exp(2j*np.pi*i/SAMPLINGS) for i in range(SAMPLINGS)])
     # samples = np.array([(1+1j) for _ in range(SAMPLINGS)]) 
-    samples = primitive_root_code(1019, 2)*2
+    samples = primitive_root_code(1019, 2) * 2
     # samples = const_powerd_samples(2, np.pi/(1+np.sqrt(2)), SAMPLINGS) * 2
     samples = np.array(samples) * 2
 
-    sdr = DEVICES.find("1044734c9605000d15003300deb64fb9ce").get_pluto()
+    sdr = DEVICES.find("9ce").get_pluto()
     sdr.tx_lo = DEFAULT_TX_LO
     sdr.tx_rf_bandwidth = DEFAULT_TX_BW
     sdr.sample_rate = SAMPLE_RATE
